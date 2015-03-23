c4ea2cd2fde225f9269d64b423762dfddedd47fe
Casper Thomsen <ct@clearhaus.com>
2015-03-23 16:40:04 +0100

    Add NilClass extension.

--- lib/monetize/core_extensions.rb
+++ lib/monetize/core_extensions.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 
+require "monetize/core_extensions/nil_class"
 require "monetize/core_extensions/numeric"
 require "monetize/core_extensions/string"
 require "monetize/core_extensions/symbol"
