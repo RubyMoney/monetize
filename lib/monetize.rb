6bb3506240ac352edfdae1b7608bc727b398bfc2
Alvaro Redondo <alvaro@redondo.name>
2015-09-02 23:56:37 +0200

    Fix Metrics/CyclomaticComplexity, PerceivedComplexity and BlockNesting

--- lib/monetize.rb
+++ lib/monetize.rb
@@ -24,7 +24,7 @@ module Monetize
     'T'    => 12
   }
   MULTIPLIER_SUFFIXES.default = 0
-  MULTIPLIER_REGEXP = Regexp.new(format('\d(%s)\b[^\d]*$', MULTIPLIER_SUFFIXES.keys.join('|')), 'i')
+  MULTIPLIER_REGEXP = Regexp.new(format('^(.*?\d)(%s)\b([^\d]*)$', MULTIPLIER_SUFFIXES.keys.join('|')), 'i')
 
   # Class methods
   class << self
@@ -89,96 +89,39 @@ module Monetize
   end
 
   def self.extract_cents(input, currency = Money.default_currency)
-    multiplier_suffix = (matches = MULTIPLIER_REGEXP.match(input)) ? matches[1].upcase : nil
-    multiplier_exp = MULTIPLIER_SUFFIXES[multiplier_suffix]
+    multiplier_exp, input = extract_multiplier(input)
 
     num = input.gsub(/[^\d.,'-]/, '')
 
-    negative = num =~ /^-|-$/ ? true : false
-
-    decimal_char = currency.decimal_mark
-
-    num = num.sub(/^-|-$/, '') if negative
-
-    fail ArgumentError, 'Invalid currency amount (hyphen)' if num.include?('-')
+    negative, num = extract_sign(num)
 
     num.chop! if num.match(/[\.|,]$/)
 
-    used_delimiters = num.scan(/[^\d]/)
-
-    case used_delimiters.uniq.length
-    when 0
-      major = num
-      minor = 0
-    when 2
-      thousands_separator, decimal_mark = used_delimiters.uniq
-
-      major, minor = num.gsub(thousands_separator, '').split(decimal_mark)
-      minor = 0 unless minor
-    when 1
-      decimal_mark = used_delimiters.first
-
-      if decimal_char == decimal_mark
-        major, minor = num.split(decimal_char)
-      else
-        if num.scan(decimal_mark).length > 1 # multiple matches; treat as decimal_mark
-          major = num.gsub(decimal_mark, '')
-          minor = 0
-        else
-          possible_major, possible_minor = num.split(decimal_mark)
-          possible_major ||= '0'
-          possible_minor ||= '00'
-
-          if possible_minor.length != 3 # thousands_separator
-            major = possible_major
-            minor = possible_minor
-          else
-            if possible_major.length > 3
-              major = possible_major
-              minor = possible_minor
-            else
-              if decimal_mark == '.'
-                major = possible_major
-                minor = possible_minor
-              else
-                major = "#{possible_major}#{possible_minor}"
-                minor = 0
-              end
-            end
-          end
-        end
-      end
-    else
-      fail ArgumentError, 'Invalid currency amount'
-    end
+    major, minor = extract_major_minor(num, currency)
 
     cents = major.to_i * currency.subunit_to_unit
 
+    cents, minor = apply_multiplier(multiplier_exp, cents, minor)
+
+    cents += set_minor_precision(minor, currency)
+
+    apply_sign(negative, cents)
+  end
+
+  private
+
+  def self.apply_multiplier(multiplier_exp, cents, minor)
     cents *= (10**multiplier_exp)
     minor = minor.to_s + ('0' * multiplier_exp)
     shift = minor[0...multiplier_exp].to_i * 100
     cents += shift
     minor = (minor[multiplier_exp..-1] || '')
-    minor = if Money.infinite_precision
-              (BigDecimal.new(minor) / (10**minor.size)) * currency.subunit_to_unit
-            elsif minor.size < currency.decimal_places
-              (minor + ('0' * currency.decimal_places))[0, currency.decimal_places].to_i
-            elsif minor.size > currency.decimal_places
-              if minor[currency.decimal_places, 1].to_i >= 5
-                minor[0, currency.decimal_places].to_i + 1
-              else
-                minor[0, currency.decimal_places].to_i
-              end
-            else
-              minor.to_i
-            end
-
-    cents += minor
-
-    negative ? cents * -1 : cents
+    [cents, minor]
   end
 
-  private
+  def self.apply_sign(negative, cents)
+    negative ? cents * -1 : cents
+  end
 
   def self.contains_currency_symbol?(amount)
     amount =~ currency_symbol_regex
@@ -193,8 +136,81 @@ module Monetize
     end
   end
 
+  def self.extract_major_minor(num, currency)
+    used_delimiters = num.scan(/[^\d]/).uniq
+
+    case used_delimiters.length
+    when 0
+      [num, 0]
+    when 2
+      thousands_separator, decimal_mark = used_delimiters
+      split_major_minor(num.gsub(thousands_separator, ''), decimal_mark)
+    when 1
+      if currency.decimal_mark == used_delimiters.first
+        split_major_minor(num, used_delimiters.first)
+      else
+        extract_major_minor_with_tentative_delimiter(num, used_delimiters.first)
+      end
+    else
+      fail ArgumentError, 'Invalid currency amount'
+    end
+  end
+
+  def self.extract_major_minor_with_tentative_delimiter(num, delimiter)
+    if num.scan(delimiter).length > 1
+      # Multiple matches; treat as thousands separator
+      [num.gsub(delimiter, ''), '00']
+    else
+      possible_major, possible_minor = split_major_minor(num, delimiter)
+
+      if possible_minor.length != 3 or possible_major.length > 3 or delimiter == '.'
+        # Doesn't look like thousands separator
+        [possible_major, possible_minor]
+      else
+        ["#{possible_major}#{possible_minor}", '00']
+      end
+    end
+  end
+
+  def self.extract_multiplier(input)
+    if (matches = MULTIPLIER_REGEXP.match(input))
+      multiplier_suffix = matches[2].upcase
+      [MULTIPLIER_SUFFIXES[multiplier_suffix], "#{$1}#{$3}"]
+    else
+      [0, input]
+    end
+  end
+
+  def self.extract_sign(input)
+    result = (input =~ /^-+(.*)$/ or input =~ /^(.*)-+$/) ? [true, $1] : [false, input]
+    fail ArgumentError, 'Invalid currency amount (hyphen)' if result[1].include?('-')
+    result
+  end
+
   def self.regex_safe_symbols
     CURRENCY_SYMBOLS.keys.map { |key| Regexp.escape(key) }.join('|')
+  end
+
+  def self.set_minor_precision(minor, currency)
+    if Money.infinite_precision
+      (BigDecimal.new(minor) / (10**minor.size)) * currency.subunit_to_unit
+    elsif minor.size < currency.decimal_places
+      (minor + ('0' * currency.decimal_places))[0, currency.decimal_places].to_i
+    elsif minor.size > currency.decimal_places
+      if minor[currency.decimal_places, 1].to_i >= 5
+        minor[0, currency.decimal_places].to_i + 1
+      else
+        minor[0, currency.decimal_places].to_i
+      end
+    else
+      minor.to_i
+    end
+  end
+
+  def self.split_major_minor(num, delimiter)
+    major, minor = num.split(delimiter)
+    minor = '00' unless minor
+    [major, minor]
   end
 
   def self.currency_symbol_regex
